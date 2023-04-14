bb0b5299bc6c1e82fbade12bb3ba392a3389a54f
annuabk <annukba@gmail.com>
2023-04-14 16:15:38 +0530

    added splashscreen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:SeekAssist/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'Home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage(
+              'assets/images/logopik.png',
+            ),
+          ),
+          Text(
+            'SeekAssist',
+            textScaleFactor: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
