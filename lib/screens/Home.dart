b0df4cf6a8a0fedb5e7cd73742213100fef0720c
DELL <abhijithj2024@cs.ajce.in>
2023-06-04 22:56:22 +0530

    voice assistant

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -9,7 +9,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_recognition_result.dart' as stt;
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:audioplayers/audioplayers.dart';
 
 
 
@@ -21,9 +20,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   FlutterTts flutterTts = FlutterTts();
-  bool isImageselected=false;
-  //AudioCache audioCache = AudioCache();
-  //AudioPlayer audioPlayer = AudioPlayer();
+ // bool isImageselected=false;
 
   List<CameraDescription>? cameras; //list out the camera available
   CameraController? controller; //controller for camera
@@ -39,7 +36,8 @@ class _HomeState extends State<Home> {
     setState(() {
       if (pickedImage != null) {
         _image = File(pickedImage.path);
-        isImageselected=true;
+        //isImageselected=true;
+        predictImage();
       } else {
         print("No Image Selected");
       }
@@ -52,7 +50,6 @@ class _HomeState extends State<Home> {
   void initState() {
     loadCamera();
     SpeechRecognition();
-   // audioCache.load('assets/soundfiles/shutter_sound.wav');
     super.initState();
   }
   loadCamera() async {
@@ -82,9 +79,7 @@ class _HomeState extends State<Home> {
             // Update the UI
           });
           if (image != null) {
-            //audioPlayer.setVolume(1.0);
-           // audioPlayer.play(UrlSource('assets/soundfiles/shutter_sound.wav'));
-            await predictImagecapture(File(image!.path));
+            predictImagecapture(File(image!.path));
           } else {
             print('No image captured');
           }
@@ -95,17 +90,7 @@ class _HomeState extends State<Home> {
     }
   }
 
-  /*void startListening() {
-    speech.listen(
-      onResult: (stt.SpeechRecognitionResult result) {
-        String spokenWords = result.recognizedWords.toLowerCase();
-        if (spokenWords.contains('capture image')) {
-          // Call the capture image function here
-          captureImage();
-        }
-      },
-    );
-  }*/
+
 
   void SpeechRecognition() async {
     bool available = await speech.initialize();
@@ -129,41 +114,6 @@ class _HomeState extends State<Home> {
 
   String? prediction;
 
-  /* Future<String> getPrediction(File imageFile) async {
-    try {
-      final url = Uri.parse('https://api.replicate.com/v1/predictions');
-      final request = http.MultipartRequest('POST', url);
-      print('Image file path: ${imageFile.path}');
-      request.files.add(await http.MultipartFile.fromPath('file', imageFile.path));
-      print('Request files: ${request.files}');
-      final response = await request.send();
-      final responseJson = jsonDecode(await response.stream.bytesToString());
-      print('Response JSON: $responseJson');
-      final predictionResult = responseJson['prediction'];
-      return predictionResult ?? "No prediction found";
-    }
-    catch (e) {
-      print('Error: $e');
-      return "Error getting prediction";
-    }
-  }
-
-
-
-  Future<void> predictImage() async {
-    try {
-      if (_image != null) {
-        final predictionResult = await getPrediction(_image!);
-        setState(() {
-          prediction = predictionResult;
-        });
-      } else {
-        print('No image selected');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }*/
   Future<void> predictImagecapture(File imagefile) async {
     if (imagefile != null) {
       final subscriptionKey = '471b2a144cee41fe8599ad4f94a8174e';
@@ -322,11 +272,11 @@ class _HomeState extends State<Home> {
                 ),
                 //display captured image
               ),
-              if(isImageselected)
-                ElevatedButton(
-                  onPressed: predictImage,
-                  child: Text('Predict Image'),
-                ),
+              // if(isImageselected)
+              //   ElevatedButton(
+              //     onPressed: predictImage,
+              //     child: Text('Predict Image'),
+              //   ),
               prediction != null
                   ? Text(
                 "Caption: $prediction",
